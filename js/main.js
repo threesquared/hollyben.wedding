937d15d083f1bbee08e9f2b2bb63192d45ae846c
Ben Speakman <ben.speakman@workday.com>
2025-11-03 18:16:43 +0000

    Updates

--- js/main.js
+++ js/main.js
@@ -99,6 +99,21 @@
 
 })(jQuery);
 
+$.fn.randomize = function(selector){
+  var $elems = selector ? $(this).find(selector) : $(this).children(),
+      $parents = $elems.parent();
+
+  $parents.each(function(){
+      $(this).children(selector).sort(function(){
+          return Math.round(Math.random()) - 0.5;
+      }).detach().appendTo(this);
+  });
+
+  return this;
+};
+
+$('.slide').randomize('.image');
+
 $(document).ready(function(){
 	$('.slide').slick({
 		centerMode: true,
